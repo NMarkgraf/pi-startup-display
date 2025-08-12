d6b88be041dcc9e5ac765ab8207fe62a9e288250
NMarkgraf <nmarkgraf@hotmail.com>
2025-08-12 10:53:44 +0200

    Update and some fixes.

--- start-display.py
+++ start-display.py
@@ -1,14 +1,16 @@
 """
-  Rsaspberry PI Zero StartUp Display für waveshare
+  Raspberry PI Zero StartUp Display für waveshare
   
   (C) by Norman Markgraf
       in 2025
       
   Release 1.0 (10. August 2025)
+  Release 1.1 (12. August 2025)
   
 """
 from PIL import Image,ImageDraw,ImageFont
 from datetime import datetime
+from time import sleep
 import socket
 import epd2in13_V4
 
@@ -23,24 +25,24 @@ dns_ip6 = "2001:4860:4860::8888"
 
 def getIP4Address(prefix = "ip4: "):
   s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  s.connect((dns_ip4, 1)) 
+  s.connect((dns_ip4, 1))
   return prefix + s.getsockname()[0]
 
 def getIP6Address(prefix = ""):
   s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-  s.connect((dns_ip6, 1)) 
-  ip6 =s.getsockname()[0].slpit(":")
+  s.connect((dns_ip6, 1))
+  ip6 =s.getsockname()[0].split(":")
   first = ":".join(ip6[0:4]) + ":"
-  second = ":".join(ip[4:8])
+  second = ":".join(ip6[4:8])
   return (prefix + first, second)
 
 def getDateTime(prefix = "Start: "):
-  return prefix + str(datetime.now()[:19])
+  return prefix + str(datetime.now())[:19]
 
 try:
 	epd = epd2in13_V4.EPD()
 	epd.init()
-	epd.Clear(0xFF)
+#	epd.Clear(0xFF)
 	
 	font10 = ImageFont.truetype('Font.ttc', 10)
 	font13 = ImageFont.truetype('Font.ttc', 13)
@@ -55,14 +57,15 @@ try:
 
 	(first, second) = getIP6Address()
 	
-	draw.text((0, 30), "ip6: " + first, font = font15, fill = 0)
-	draw.text((0, 48), "     " + secound, font = font15, fill = 0)
+	draw.text((0, 48), "ip6: " font = font25, fill = 0)
+	draw.text((20, 30), first, font = font15, fill = 0)
+	draw.text((20, 48), second, font = font15, fill = 0)
 
 	draw.text((0, 68), getIP4Address(), font = font25, fill = 0)
 
 	epd.display(epd.getbuffer(image))
 
-  time.sleep(10)
+  sleep(10)
   
   
 except IOError as e:
@@ -71,8 +74,3 @@ except IOError as e:
 except KeyboardInterrupt:    
 	epd2in13_V4.epdconfig.module_exit(cleanup=True)
 	exit(0)
-	
-except:
-  exit(0)
-
-exit(0)
