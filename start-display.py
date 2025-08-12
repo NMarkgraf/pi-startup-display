ac36242e2e2289db9a030231696f363f74284210
NMarkgraf <nmarkgraf@hotmail.com>
2025-08-12 11:36:40 +0200

    Vielleicht behebe ich so die Threading Exception, in dem ich die sockets mit close() schliesse. ,-)

--- start-display.py
+++ start-display.py
@@ -26,12 +26,15 @@ dns_ip6 = "2001:4860:4860::8888"
 def getIP4Address(prefix = "ip4: "):
   s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   s.connect((dns_ip4, 1))
-  return prefix + s.getsockname()[0]
+  ip4 = s.getsockname()[0]
+  s.colose()
+  return prefix + ip4
 
 def getIP6Address(prefix = ""):
   s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
   s.connect((dns_ip6, 1))
   ip6 =s.getsockname()[0].split(":")
+  s.close()
   first = ":".join(ip6[0:4]) + ":"
   second = ":".join(ip6[4:8])
   return (prefix + first, second)
@@ -44,10 +47,10 @@ try:
 	epd.init()
 #	epd.Clear(0xFF)
 	
-	font10 = ImageFont.truetype('Font.ttc', 10)
-	font13 = ImageFont.truetype('Font.ttc', 13)
+#	font10 = ImageFont.truetype('Font.ttc', 10)
+#	font13 = ImageFont.truetype('Font.ttc', 13)
 	font15 = ImageFont.truetype('Font.ttc', 15)
-	font20 = ImageFont.truetype('Font.ttc', 20)
+#	font20 = ImageFont.truetype('Font.ttc', 20)
 	font25 = ImageFont.truetype('Font.ttc', 25)
 	
 	image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame    
@@ -57,11 +60,11 @@ try:
 
 	(first, second) = getIP6Address()
 	
-	draw.text((0, 32), "ip6:", font = font25, fill = 0)
-	draw.text((50, 30), first, font = font15, fill = 0)
-	draw.text((50, 48), second, font = font15, fill = 0)
+	draw.text((0, 30), "ip6:", font = font25, fill = 0)
+	draw.text((50, 28), first, font = font15, fill = 0)
+	draw.text((50, 46), second, font = font15, fill = 0)
 
-	draw.text((0, 68), getIP4Address(), font = font25, fill = 0)
+	draw.text((0, 70), getIP4Address(), font = font25, fill = 0)
 
 	epd.display(epd.getbuffer(image))
 	sleep(10)
