cdb8111c7dcc3c4561d27b63fd1726659c585178
NMarkgraf <nmarkgraf@hotmail.com>
2025-08-12 11:42:46 +0200

    Typos fixed!

--- start-display.py
+++ start-display.py
@@ -8,7 +8,8 @@
   Release 1.1 (12. August 2025)
   
 """
-from PIL import Image,ImageDraw,ImageFont
+
+from PIL import Image, ImageDraw, ImageFont
 from datetime import datetime
 from time import sleep
 import socket
@@ -19,16 +20,17 @@ dns_ip4 = "8.8.8.8"
 dns_ip6 = "2001:4860:4860::8888"
 
 # Cloudflare DNS
-#dns_ip4 = "1.1.1.2"
-#dns_ip6 = "2606:4700:4700::1111"
+# dns_ip4 = "1.1.1.2"
+# dns_ip6 = "2606:4700:4700::1111"
 
 
 def getIP4Address(prefix = "ip4: "):
   s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   s.connect((dns_ip4, 1))
   ip4 = s.getsockname()[0]
-  s.colose()
+  s.close()
   return prefix + ip4
+
 
 def getIP6Address(prefix = ""):
   s = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
@@ -39,8 +41,10 @@ def getIP6Address(prefix = ""):
   second = ":".join(ip6[4:8])
   return (prefix + first, second)
 
+
 def getDateTime(prefix = "Start: "):
   return prefix + str(datetime.now())[:19]
+
 
 try:
 	epd = epd2in13_V4.EPD()
@@ -50,13 +54,14 @@ try:
 #	font10 = ImageFont.truetype('Font.ttc', 10)
 #	font13 = ImageFont.truetype('Font.ttc', 13)
 	font15 = ImageFont.truetype('Font.ttc', 15)
+	font18 = ImageFont.truetype('Font.ttc', 18)
 #	font20 = ImageFont.truetype('Font.ttc', 20)
 	font25 = ImageFont.truetype('Font.ttc', 25)
 	
 	image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame    
 	
 	draw = ImageDraw.Draw(image)
-	draw.text((0, 3), getDateTime(), font = font15, fill = 0)
+	draw.text((0, 3), getDateTime(), font = font18, fill = 0)
 
 	(first, second) = getIP6Address()
 	
