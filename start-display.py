5d7d894beaddae6ffdec1a19c0b5242a694ed637
NMarkgraf <nmarkgraf@hotmail.com>
2025-08-13 08:51:02 +0200

    Mehr mache ich da erstmal nicht.

--- start-display.py
+++ start-display.py
@@ -46,38 +46,41 @@ def getDateTime(prefix = "Start: "):
   return prefix + str(datetime.now())[:19]
 
 
-try:
-	epd = epd2in13_V4.EPD()
-	epd.init()
-#	epd.Clear(0xFF)
-	
-#	font10 = ImageFont.truetype('Font.ttc', 10)
-#	font13 = ImageFont.truetype('Font.ttc', 13)
-	font15 = ImageFont.truetype('Font.ttc', 15)
-	font18 = ImageFont.truetype('Font.ttc', 18)
-#	font20 = ImageFont.truetype('Font.ttc', 20)
-	font25 = ImageFont.truetype('Font.ttc', 25)
-	
-	image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame    
-	
-	draw = ImageDraw.Draw(image)
-	draw.text((0, 3), getDateTime(), font = font18, fill = 0)
-
-	(first, second) = getIP6Address()
-	
-	draw.text((0, 30), "ip6:", font = font25, fill = 0)
-	draw.text((50, 28), first, font = font15, fill = 0)
-	draw.text((50, 46), second, font = font15, fill = 0)
-
-	draw.text((0, 70), getIP4Address(), font = font25, fill = 0)
-
-	epd.display(epd.getbuffer(image))
-	sleep(10)
+if __name__ == '__main__':
   
   
-except IOError as e:
-	exit(0)
- 
-except KeyboardInterrupt:    
-	epd2in13_V4.epdconfig.module_exit(cleanup=True)
-	exit(0)
+  try:
+  	epd = epd2in13_V4.EPD()
+  	epd.init()
+  #	epd.Clear(0xFF)
+  	
+  #	font10 = ImageFont.truetype('Font.ttc', 10)
+  #	font13 = ImageFont.truetype('Font.ttc', 13)
+  	font15 = ImageFont.truetype('Font.ttc', 15)
+  	font18 = ImageFont.truetype('Font.ttc', 18)
+  #	font20 = ImageFont.truetype('Font.ttc', 20)
+  	font25 = ImageFont.truetype('Font.ttc', 25)
+  	
+  	image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame    
+  	
+  	draw = ImageDraw.Draw(image)
+  	draw.text((0, 3), getDateTime(), font = font18, fill = 0)
+  
+  	(first, second) = getIP6Address()
+  	
+  	draw.text((0, 30), "ip6:", font = font25, fill = 0)
+  	draw.text((50, 28), first, font = font15, fill = 0)
+  	draw.text((50, 46), second, font = font15, fill = 0)
+  
+  	draw.text((0, 70), getIP4Address(), font = font25, fill = 0)
+  
+  	epd.display(epd.getbuffer(image))
+  	sleep(10)
+    
+    
+  except IOError as e:
+  	exit(0)
+   
+  except KeyboardInterrupt:    
+  	epd2in13_V4.epdconfig.module_exit(cleanup=True)
+  	exit(0)
