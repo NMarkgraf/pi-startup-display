5f2c273a3f7ec119b06c1dae8619bfb510a39ae6
NMarkgraf <nmarkgraf@hotmail.com>
2025-11-15 19:38:24 +0100

    Neue Version 1.5

--- start-display.py
+++ start-display.py
@@ -9,6 +9,7 @@
   Release 1.2 ( 9. September 2025)
   Release 1.3 (11. September 2025)
   Release 1.4 (24. Oktober 2025)
+  Release 1.5 (15. November 2025)
 """
 
 from PIL import Image, ImageDraw, ImageFont
@@ -18,36 +19,37 @@ import psutil
 import socket
 import epd2in13_V4
 import logging
+from systemd.journal import JournalHandler 
 
 # Logger einrichten
 logger = logging.getLogger(__name__)
+logger.addHandler(JournalHandler(SYSLOG_IDENTIFIER="start-display"))
 
-# Google DNS, Cloudflare DNS
+# [Google DNS, Cloudflare DNS]
 dns_ip4 = ["8.8.8.8", "1.1.1.2"]
 dns_ip6 = ["2001:4860:4860::8888", "2606:4700:4700::1111"]
 
 
 def getAllIPAdresses():
-  interfaces = []
   ip_list = []
   ip4_set = set()
   ip6_set = set()
+  logger.debug(psutil.net_if_addrs())
   for interface in psutil.net_if_addrs():
-    if interface == "lo":
-      continue
-    interface_addrs = psutil.net_if_addrs().get(interface)
-    for snicaddr in interface_addrs:
-        if snicaddr.family in(socket.AF_INET, socket.AF_INET6):
-            if "%" not in snicaddr.address:
-              ip_list += snicaddr.address,
-    for ip in ip_list:
-      if ":" in ip:
-        ip6_set.add(ip)
-      if "." in ip:
-        ip4_set.add(ip)
-  logger.debug(ip_list)
-  logger.debug(list(ip4_set))
-  logger.debug(list(ip6_set))
+    if interface != "lo":
+      interface_addrs = psutil.net_if_addrs().get(interface)
+      for snicaddr in interface_addrs:
+          if snicaddr.family in(socket.AF_INET, socket.AF_INET6):
+#              if "%" not in snicaddr.address:
+            ip_list += snicaddr.address,
+      for ip in ip_list:
+        if ":" in ip:
+          ip6_set.add(ip)
+        if "." in ip:
+          ip4_set.add(ip)
+  logger.info(ip_list)
+  logger.info(list(ip4_set))
+  logger.info(list(ip6_set))
   return (list(ip4_set), list(ip6_set), ip_list)
 
 
@@ -87,10 +89,10 @@ def pushImage(dsp, image):
 
 
 if __name__ == '__main__':
-  logging.basicConfig(filename='start-display.log', level=logging.INFO)
-  logger.info('Started at:'+getDateTime(""))
+  logging.basicConfig(filename='start-display.log', level=logging.DEBUG)
+  logger.info('Started at '+getDateTime(""))
   counter = 0
-  
+
   try:
     display = initDisplay()
     image = initImage(display)
@@ -104,28 +106,31 @@ if __name__ == '__main__':
     
     pushImage(display, image)
     
-    sleep(10)
-    logger.info('End sleeping!')
+    logger.debug('Start sleeping ...')
+    sleep(20)
+    logger.debug('End sleeping!')
 
     while True:
       image = initImage(display)
       draw = initDraw(image)
-      logger.debug('draw and image initialised!')
+      logger.debug('draw and  image initialised!')
       
       drawText(draw, (0,3), getDateTime(), fonts[18])
       
+      logger.info("Get All IP Adresses")
       (ip4_l, ip6_l, x) = getAllIPAdresses()
-      logger.debug("Get All IP Adresses")
 
       ip4_len = len(ip4_l)
       ip6_len = len(ip6_l)
-      #max = ip4_len * ip6_len
+
       if ip4_len == 0:
         ip4_l = ["0.0.0.0"]
         ip4_len = 1
+        
       if ip6_len == 0:
         ip6_l = ["0:0:0:0:0:0:0:0"]
         ip6_len = 1
+
       max = ip4_len * ip6_len
       
       ip4 = ip4_l[counter % ip4_len]
@@ -133,7 +138,7 @@ if __name__ == '__main__':
       first = ":".join(ip6[0:4]) + ":"
       second = ":".join(ip6[4:8])
 
-      logger.info("ip6: "+ first + second)
+      logger.info("ip6: " + first + second)
       
       drawText(draw, (0, 30), "ip6:", fonts[25])
       drawText(draw, (50, 28), first, fonts[15])
@@ -144,7 +149,7 @@ if __name__ == '__main__':
       
       pushImage(display, image)
       
-      logger.debug("Sleeping....")
+      logger.debug("Start sleeping ...")
       sleep(100)
       counter += 1
       counter %= max
@@ -152,12 +157,12 @@ if __name__ == '__main__':
 
     
   except IOError as e:
-    logger.error('Finished with IOError')
+    logger.info('Finished with IOError at '+getDateTime(""))
     exit(0)
    
   except KeyboardInterrupt: 
-    logger.error('Finished with KeyboardInterrupt')
+    logger.info('Finished with KeyboardInterrupt at '+getDateTime(""))
     epd2in13_V4.epdconfig.module_exit(cleanup=True)
     exit(0)
       
-  logger.info('Finished')
+  logger.info('Finished at '+getDateTime(""))
